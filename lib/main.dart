66490816fb35ba04ebf51899e1e7fced76f13435
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-07-14 15:35:43 -0400

    getting http response

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,35 @@ import 'dart:convert';
 
 import 'package:coincap/models/app_config.dart';
 import 'package:coincap/pages/home_page.dart';
+import 'package:coincap/services/http_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
+  registerHTTPService();
   runApp(const MyApp());
+}
 
-  Future<void> loadConfig() async {
-    String _configCOntent =
-        await rootBundle.loadString("assets/config/main.json");
+Future<void> loadConfig() async {
+  String _configContent =
+      await rootBundle.loadString("assets/config/main.json");
 
-    Map _configData = jsonDecode(_configCOntent);
-    print(_configData);
-    GetIt.instance.registerSingleton<AppConfig>(
-      AppConfig(
-        COIN_API_BASE_URL: _configData["COIN_API_BASE_URL"],
-      ),
-    );
-  }
+  Map _configData = jsonDecode(_configContent);
+  print(_configData);
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(
+      COIN_API_BASE_URL: _configData["COIN_API_BASE_URL"],
+    ),
+  );
+}
+
+void registerHTTPService() {
+  GetIt.instance.registerSingleton<HTTPService>(
+    HTTPService(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -30,6 +39,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CoinCap',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
