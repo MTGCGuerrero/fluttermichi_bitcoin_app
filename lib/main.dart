d840d6ddd4f67e501d520421ecf57f84441d080e
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-07-14 13:24:27 -0400

    add config

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
+import 'dart:convert';
+
+import 'package:coincap/models/app_config.dart';
 import 'package:coincap/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+
+  Future<void> loadConfig() async {
+    String _configCOntent =
+        await rootBundle.loadString("assets/config/main.json");
+
+    Map _configData = jsonDecode(_configCOntent);
+    print(_configData);
+    GetIt.instance.registerSingleton<AppConfig>(
+      AppConfig(
+        COIN_API_BASE_URL: _configData["COIN_API_BASE_URL"],
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
